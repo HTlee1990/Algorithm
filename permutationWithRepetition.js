41ba492fb672ab497a26f64c3581027184800fc9
Mark <notbibu@gmail.com>
2021-08-12 08:08:44 +0900

    Permutation & Combination

--- permutationWithRepetition.js
+++ permutationWithRepetition.js
@@ -1,14 +1,17 @@
+//중복이 가능한 경우의수를 구하는 중복순열 문제
+//가위, 바위, 보를 round판 했을떄 나올 수 있는 모든 경우의 수를 정리.
 function rockPaperScissors (round) {
-    // TODO: 여기에 코드를 작성합니다.
     const result = [];
     const rps = ["rock", "paper", "scissors"]
     round = round || 3
     
     const aux = (count, temp) => {
+      //재귀 종료
       if(count === round){
         result.push(temp);
         return;
       }
+      
       aux(count+1, temp.concat(rps[0]))
       aux(count+1, temp.concat(rps[1]))
       aux(count+1, temp.concat(rps[2]))
