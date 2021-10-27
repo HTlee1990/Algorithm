027e79a80770b515db6d9daf800e90fb66080927
Mark <notbibu@gmail.com>
2021-10-27 22:16:38 +0900

    [Algo]35.PG

--- 34.PG(이분탐색 - 징검다리)/34.PG(이분탐색 - 징검다리).js
+++ 34.PG(이분탐색 - 징검다리)/34.PG(이분탐색 - 징검다리).js
@@ -1,3 +1,5 @@
+// https://programmers.co.kr/learn/courses/30/lessons/43236
+
 function solution(distance, rocks, n) {
   rocks.sort((a, b) => a - b);
   let min = 1;
---
+++ 35.PG(week5 - 모음사전)/35.PG(week5 - 모음사전).js
@@ -0,0 +1,12 @@
+function solution(word) {
+  var answer = 0;
+  return answer;
+}
+
+//주어진 단어가 몇번째 단어인지 리턴
+//   A, AA, AAA, AAAA, AAAAA  => 5개
+//   AAAAE, AAAAI, AAAAO, AAAAU => 4개
+//   AAAE, AAAEA, AAAEE, AAAEI, AAAEO => 5개
+//   AAAO, AAAOA, AAAOE, AAAOI, AAAOO => 5개
+
+//AAAE를 찾고 싶다면???
