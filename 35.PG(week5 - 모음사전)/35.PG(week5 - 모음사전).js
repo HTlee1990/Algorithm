221ee4f9c6e19dfd8c0461a01aca8d96c33b404f
Mark <notbibu@gmail.com>
2021-10-28 11:30:01 +0900

    [Algo]35.PG

--- 35.PG(week5 - 모음사전)/35.PG(week5 - 모음사전).js
+++ 35.PG(week5 - 모음사전)/35.PG(week5 - 모음사전).js
@@ -1,12 +1,17 @@
 function solution(word) {
   var answer = 0;
-  return answer;
+  const set = { A: 0, E: 1, I: 2, O: 3, U: 4 };
+  const weight = [781, 156, 31, 6, 1];
+  word.split("").forEach((w, idx) => {
+    if (w !== "A") {
+      answer += weight[idx] * set[w];
+    }
+  });
+  return word.length + answer;
 }
 
-//주어진 단어가 몇번째 단어인지 리턴
-//   A, AA, AAA, AAAA, AAAAA  => 5개
-//   AAAAE, AAAAI, AAAAO, AAAAU => 4개
-//   AAAE, AAAEA, AAAEE, AAAEI, AAAEO => 5개
-//   AAAO, AAAOA, AAAOE, AAAOI, AAAOO => 5개
+// function solution(words) {
+//     return words.split('').reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
+// }
 
-//AAAE를 찾고 싶다면???
+// 아 각 자릿수에 대해 생기는 경우의 수가 첫 번째 배열이고 사전 순으로 나열되니까 앞에 있는 경우의 수 건너뛰려고 인덱스 곱해주고 중간에 'A' 문자가 한 번씩 끼니까 +1을 해주는 거구나..
