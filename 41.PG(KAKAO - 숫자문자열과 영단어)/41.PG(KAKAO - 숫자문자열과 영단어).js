b91435aa52a2c6d287d4bfbe7a0f497c71585b26
Mark <notbibu@gmail.com>
2021-11-07 23:05:04 +0900

    [Algo]Fixed

--- 41.PG(KAKAO - 숫자문자열과 영단어)/41.PG(KAKAO - 숫자문자열과 영단어).js
+++ 41.PG(KAKAO - 숫자문자열과 영단어)/41.PG(KAKAO - 숫자문자열과 영단어).js
@@ -16,18 +16,21 @@ function solution(s) {
     s = s.replace(new RegExp(`${table[i]}`, "g"), i);
   }
   return Number(s);
+  // var answer = [];
   // let numTable = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
   // let strTable = {zero: 0, one: 1, two: 2, three: 3, four:4, five: 5, six: 6, seven: 7, eight: 8, nine: 9}
   // let bucket = '';
   // s.split('').forEach((el, idx) => {
-  //     if(numTable[el]){
+  //     if(numTable[el] !== undefined){
   //         answer.push(numTable[el]);
   //     }else{
   //         bucket = bucket.concat(el);
-  //         if(strTable[bucket]){
+  //         if(strTable[bucket] !== undefined){
   //             answer.push(strTable[bucket]);
   //             bucket='';
   //         }
   //     }
   // })
+
+  // return +answer.join('')
 }
