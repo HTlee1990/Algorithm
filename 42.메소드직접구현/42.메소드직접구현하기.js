1a73425a8ade2111285ac8ad6f5307efd6390a70
Mark <notbibu@gmail.com>
2021-11-08 14:54:47 +0900

    [Algo]42.메소드를 직접 구현해보자

---
+++ 42.메소드직접구현/42.메소드직접구현하기.js
@@ -0,0 +1,36 @@
+let arr = [1, 2, 3, 4, 5];
+
+//forEach 만들어보기
+Array.prototype.myforEach = function (callback, context) {
+  for (let i = 0; i < this.length; i++) {
+    //만약, forEach에서 2번째 인자로 this를 지정해서 보내줬다면, context값을 넣고, 그렇지 않으면 null
+    callback.call(context || null, this[i], i, this);
+  }
+};
+
+arr.myforEach((el, idx, self) => {
+  console.log(el, idx, self);
+});
+
+//map 만들어보기
+Array.prototype.myMap = function (callback, context) {
+  const arr = [];
+  for (let i = 0; i < this.length; i++) {
+    arr.push(callback.call(context || null, this[i], i, this));
+  }
+  return arr;
+};
+
+console.log(arr.myMap((el, idx) => el * idx));
+
+//reduce 만들어보기
+Array.prototype.myReduce = function (callback, initialValue) {
+  let answer = initialValue || this[0];
+  for (let i = initialValue ? 0 : 1; i < this.length; i++) {
+    let acc = answer;
+    answer = callback(acc, this[i], i, this);
+  }
+  return answer;
+};
+
+console.log(arr.myReduce((a, c) => a + c));
