1ba93d5045036c085a3455bd08faec2aa51caa0e
Mark <notbibu@gmail.com>
2021-11-12 11:51:30 +0900

    [Algo]PG.44

--- 44.PG([카카오 인턴] 키패드 누르기)/44.PG([카카오 인턴] 키패드 누르기).js
+++ 44.PG([카카오 인턴] 키패드 누르기)/44.PG([카카오 인턴] 키패드 누르기).js
@@ -1,67 +1,55 @@
 function solution(numbers, hand) {
   var answer = "";
-  const queue = [];
+
+  function getDis(cur, target) {
+    let dis = 0;
+    dis += Math.abs(cur[0] - target[0]);
+    dis += Math.abs(cur[1] - target[1]);
+    return dis;
+  }
+
   const lSet = { 1: "L", 4: "L", 7: "L" };
   const rSet = { 3: "R", 6: "R", 9: "R" };
-  let initial = 1;
-  while (queue.length > 0 || initial) {
-    if (initial) {
-      const first = numbers.shift();
-      queue.push({ target: first, left: "*", right: "#" });
-      initial = 0;
-      continue;
-    }
-    const { target, left, right } = queue.shift();
+  const map = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    "*": [3, 0],
+    0: [3, 1],
+    "#": [3, 2],
+  };
+
+  let left = "*",
+    right = "#";
+  for (let i = 0; i < numbers.length; i++) {
+    let target = numbers[i];
     if (lSet[target]) {
       answer += "L";
-      queue.push({ target, left: target, right: "#" });
+      continue;
+    }
+    if (rSet[target]) {
+      answer += "R";
+    } else {
+      const fromLeft = getDis(map[left], map[target]);
+      const fromRight = getDis(map[right], map[target]);
+      //어떤 손 쓸지 정하기
+      const whichHand =
+        fromLeft < fromRight
+          ? "L"
+          : fromLeft > fromRight
+          ? "R"
+          : hand === "left"
+          ? "L"
+          : "R";
+      answer += whichHand;
     }
   }
 
   return answer;
 }
-
-let temp = 5;
-const getDis = (cur, des, count, visited) => {
-  if (cur === des) {
-    temp = temp >= count ? count : temp;
-    return;
-  }
-  if (count > 4) return;
-  let numbers = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    ["*", 0, "#"],
-  ];
-  let row = cur <= 3 ? 0 : cur <= 6 ? 1 : cur <= 9 ? 2 : 3;
-  let col = cur === 0 ? 1 : cur % 3 === 1 ? 0 : cur % 3 === 2 ? 1 : 2;
-  visited[numbers[row][col]] = 1;
-  const direction = [
-    [-1, 0],
-    [+1, 0],
-    [0, -1],
-    [0, +1],
-  ];
-
-  const possible = [];
-  direction.forEach((dir) => {
-    let [dr, dc] = dir;
-    if (
-      row + dr >= 0 &&
-      row + dr <= 3 &&
-      col + dc >= 0 &&
-      col + dc <= 2 &&
-      !visited[numbers[row + dr][col + dc]]
-    ) {
-      possible.push([row + dr, col + dc]);
-    }
-  });
-
-  for (num of possible) {
-    if (!visited[numbers[num[0]][num[1]]]) {
-      visited[numbers[num[0]][num[1]]] = 1;
-      getDis(numbers[num[0]][num[1]], des, count + 1, visited.slice());
-    }
-  }
-};
