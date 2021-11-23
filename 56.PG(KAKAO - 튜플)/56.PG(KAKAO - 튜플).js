5922145b673f8604bc73ae85177169e7f0193e47
Mark <notbibu@gmail.com>
2021-11-23 15:51:50 +0900

    [Algo]KAKAO - 튜플

--- 56.PG(KAKAO - 튜플)/56.PG(KAKAO - 튜플).js
+++ 56.PG(KAKAO - 튜플)/56.PG(KAKAO - 튜플).js
@@ -1,27 +1,29 @@
 function solution(s) {
-  var first = []
+  let answer = []
+  let obj = {}
   //그냥 길이가 가장 긴거 뽑아서, 처리하면 되지 않을까?
   // => 순서가 중요!
+  // 순서는 어떻게 정해질까?? => 가장 빈도가 높은 순서대로
   //1. 우선 가장 큰 {}를 제거하자.
   //1-1. ','를 기준으로 split
-  let arr = s.slice(2, s.length - 2).split("},{")
+  let arr = s
+    .slice(2, s.length - 2)
+    .split("},{")
+    .map((el) => el.split(","))
 
-  //가장 긴 길이의 튜플을 result에 저장.
-  let result = arr.reduce((a, c) => {
-    let sth = c.split(",")
-    if (sth.length === 1) first.push(...sth)
-    if (a.length < sth.length) return sth
-    else return a
-  }, "")
-  // const idx = result.findIndex(item => item === answer[0]);
-  // console.log(result, answer)
-
-  //     result.split(',').forEach(el => {
-  //         if(answer.length === 0) answer.push(el)
-  //         if(!el === answer[0]) answer.push(el)
-
-  //     })
-  let answer = result.filter((el) => el === !answer[0])
-  answer.push(first[0])
-  return answer
+  //몇번 등장했는지 세어보자.
+  arr.forEach((numbers) => {
+    for (let num of numbers) {
+      if (obj[num]) {
+        obj[num]++
+      } else {
+        obj[num] = 1
+      }
+    }
+  })
+  for (let key in obj) {
+    answer.push([key, obj[key]])
+  }
+  let result = answer.sort((a, b) => b[1] - a[1]).map((el) => Number(el[0]))
+  return result
 }
