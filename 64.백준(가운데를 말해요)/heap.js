3c119b64e427fb833723ce1293833acbb3a18554
HTlee1990 <notbibu@gmail.com>
2022-03-16 23:57:49 +0900

    [Algo] programmers skill up - 인형뽑기

--- 64.백준(가운데를 말해요)/heap.js
+++ 64.백준(가운데를 말해요)/heap.js
@@ -1,9 +1,9 @@
 // let input = require("fs").readFileSync("eg.txt").toString().trim().split("\n");
-// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-//출력시, 한줄에 하나씩 말해야 하는 수를 출력해야 한다.
-// const N = +input.shift();
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// //출력시, 한줄에 하나씩 말해야 하는 수를 출력해야 한다.
+const N = +input.shift();
 // console.log("input is ", input);
-const input = ["1", "5", "2", "10", "-99", "7", "5"];
+// const input = ["1", "5", "2", "10", "-99", "7", "5"];
 class Heap {
   constructor(func) {
     this.heap = [];
@@ -12,6 +12,22 @@ class Heap {
       this.func = (a, b) => a - b;
     }
   }
+
+  removeTop() {
+    if (this.size() === 0) return null;
+    //root 인덱스 삭제
+    const top = this.heap.shift();
+    //마지막 인덱스 삭제
+    const last = this.heap.pop();
+    if (this.size() !== 0) {
+      //last를 0번째 인덱스에 넣고; 다시 정렬
+      this.insert(last);
+      this.upHeap(this.size() - 1);
+      // this.heap[0] = last;
+      // this.downHeap(0);
+    }
+  }
+
   parent(index) {
     return Math.floor((index - 1) / 2);
   }
@@ -46,11 +62,11 @@ class Heap {
       //func에 따라 작은 값 혹은 큰 값을 찾는다.
       let target;
       if (!this.hasRight(pos)) target = left;
-      else if (this.func(this.heap[left], this.heap[right]) >= 0) target = left;
+      else if (this.func(this.heap[left], this.heap[right]) > 0) target = left;
       else target = right;
 
       //만약, parent가 더 작다면, swap 실행하고, 자식노드를 부모노드로
-      if (this.func(this.heap[target], this.heap[pos]) >= 0) {
+      if (this.func(this.heap[target], this.heap[pos]) > 0) {
         this.swap(pos, target);
         pos = target;
       } else {
@@ -62,7 +78,7 @@ class Heap {
   upHeap(pos) {
     while (pos !== 0) {
       let p = this.parent(pos);
-      if (this.func(this.heap[p], this.heap[pos]) < 0) {
+      if (this.func(this.heap[p], this.heap[pos]) > 0) {
         this.swap(pos, p);
         pos = p;
       } else {
@@ -80,97 +96,43 @@ class Heap {
     // this.heap.push(value);
     // this.upHeap(this.size() - 1);
   }
+
+  top() {
+    return this.heap[0];
+  }
 }
 
-const h = new Heap((a, b) => b - a);
-
-input.forEach((el) => {
-  h.insert(+el);
-});
-console.log(h.heap);
-
-// class Heap {
-//   constructor() {
-//     //0번째 index는 필요 없는 요소
-//     this.minHeap = [];
-//     this.maxHeap = [];
-//   }
-//   //부모노드 인덱스 구하는 메소드
-//   getParent(index) {
-//     return Math.floor(index / 2);
-//   }
-//   getChild(index, arr) {
-//     const [left, right] = [index * 2 + 1, index * 2 + 2];
-//     if (left > arr.length - 1) return undefined;
-//     //left만 있고, right는 없는 경우 바로 return left
-//     if (right > arr.length - 1) return left;
-//     return arr[left] > arr[right] ? left : right;
-//   }
-//   getSmaller(index, arr) {
-//     const [left, right] = [index * 2 + 1, index * 2 + 2];
-//     if (left > arr.length - 1) return undefined;
-//     if (right > arr.length - 1) return left;
-//     return arr[left] < arr[right] ? left : right;
-//   }
-
-//   //부모 노드와 자식노드 비교하여 스왑 until 더이상 자식 노드가 크지 않을 때 까지.
-//   maxHeapify(parent) {
-//     //sort maxHeap
-//     //1. 자식노드 중 큰 값을 root노드와 비교
-//     let biggerChild = this.getChild(parent, this.maxHeap);
-//     //2. 자식노드가 부모노드보다 크다면 스왑, until it doens't
-//     while (this.maxHeap[parent] < this.maxHeap[biggerChild]) {
-//       //2-1. 스왑
-//       [this.maxHeap[parent], this.maxHeap[biggerChild]] = [
-//         this.maxHeap[biggerChild],
-//         this.maxHeap[parent],
-//       ];
-//       //2-2. 자식노드를 부모노드로, 자식노드 index는 새로 구하기
-//       [parent, biggerChild] = [
-//         biggerChild,
-//         this.getChild(biggerChild, this.maxHeap),
-//       ];
-//     }
-//   }
-//   minHeapify(parent) {
-//     let smallChild = this.getSmaller(parent, this.minHeap);
-//     //2. 자식노드가 부모노드보다 크다면 스왑, until it doens't
-//     while (this.minHeap[parent] > this.minHeap[smallChild]) {
-//       //2-1. 스왑
-//       [this.minHeap[parent], this.minHeap[smallChild]] = [
-//         this.minHeap[smallChild],
-//         this.minHeap[parent],
-//       ];
-//       //2-2. 자식노드를 부모노드로, 자식노드 index는 새로 구하기
-//       [parent, smallChild] = [
-//         smallChild,
-//         this.getSmaller(smallChild, this.minHeap),
-//       ];
-//     }
-//   }
-//   //heap에 요소를 push해주는 메소드
-//   insertEl(el) {
-//     //1. maxHeap.length 는 minHeap보다 1크거나 같다. 즉, 두 길이의 차가 1보다 크다면, minHeap에 넣자.
-//     if (this.maxHeap.length - this.minHeap.length >= 1) {
-//       this.minHeap.unshift(el);
-//     } else {
-//       this.maxHeap.unshift(el);
-//     }
-//     //2. minHeap의 모든 요소는 maxHeap의 탑 보다 크다. 즉, minHeap의 탑은 maxHeap의 탑보다 항상 커야 한다.
-//     //   만약 작다면,
-//     if (this.maxHeap[0] > this.minHeap[0]) {
-//       // 스왑
-//       [this.maxHeap[0], this.minHeap[0]] = [this.minHeap[0], this.maxHeap[0]];
-//     }
-//     //3.heapifiy를 두 배열에 모두 시행.
-//     this.maxHeapify(0);
-//     this.minHeapify(0);
-//     console.log(this.maxHeap[0]);
-//   }
-// }
-
 // const h = new Heap();
-
+// const h = new Heap((a, b) => b - a);
 // input.forEach((el) => {
-//   h.insertEl(+el);
+//   h.insert(+el);
 // });
+
+function solution() {
+  const maxQ = new Heap();
+  const minQ = new Heap((a, b) => b - a);
+
+  for (let i = 0; i < input.length; i++) {
+    const cur = input[i];
+
+    if (maxQ.size() > minQ.size()) {
+      minQ.insert(cur);
+    } else {
+      maxQ.insert(cur);
+    }
+
+    if (minQ.size() !== 0) {
+      if (minQ.top() < maxQ.top()) {
+        const minTop = minQ.removeTop();
+        const maxTop = minQ.removeTop();
+
+        minQ.insert(maxTop);
+        maxQ.insert(minTop);
+      }
+    }
+
+    console.log(maxQ.top());
+  }
+}
+
+solution();
