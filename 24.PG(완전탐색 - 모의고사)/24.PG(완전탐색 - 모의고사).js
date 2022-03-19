4c0c0f79dbc0c4ba7a5036946dfb65dbccb9b4ec
HTlee1990 <notbibu@gmail.com>
2022-03-19 20:51:32 +0900

    [Algo] 복습

--- 24.PG(완전탐색 - 모의고사)/24.PG(완전탐색 - 모의고사).js
+++ 24.PG(완전탐색 - 모의고사)/24.PG(완전탐색 - 모의고사).js
@@ -23,3 +23,24 @@ function solution(answers) {
 
   //   return answer.map((n, i) => n === max && i + 1).filter((el) => el !== false);
 }
+
+function solution(answers) {
+  let answer = [];
+  const count = [0, 0, 0];
+  const arr1 = [1, 2, 3, 4, 5];
+  const arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  answers.forEach((a, idx) => {
+    if (arr1[idx % arr1.length] === a) count[0] ? count[0]++ : (count[0] = 1);
+    if (arr2[idx % arr2.length] === a) count[1] ? count[1]++ : (count[1] = 1);
+    if (arr3[idx % arr3.length] === a) count[2] ? count[2]++ : (count[2] = 1);
+  });
+
+  const max = Math.max(...count);
+  count.forEach((c, idx) => {
+    if (c === max) answer.push(idx + 1);
+  });
+
+  return answer;
+}
