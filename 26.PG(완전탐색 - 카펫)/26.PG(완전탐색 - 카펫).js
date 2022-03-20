893f56ce856715be1cbd3b85a804f247da2a0218
HTlee1990 <notbibu@gmail.com>
2022-03-20 12:21:02 +0900

    [Algo] 완전탐색 - 카펫 복습

--- 26.PG(완전탐색 - 카펫)/26.PG(완전탐색 - 카펫).js
+++ 26.PG(완전탐색 - 카펫)/26.PG(완전탐색 - 카펫).js
@@ -14,3 +14,15 @@ function solution(brown, yellow) {
 //1. 각 격자의수 더하기. (총 격자의수)
 //2. 해당 격자의 수로 만들 수 있는 모든 사각형 모양 [] 넣기
 //3. 어떤게 적절한 사각형인지 체크.
+
+// 5개월 뒤 푼 코드 소름돋네 똑같네 ㅋㅋ
+
+function solution(brown, yellow) {
+  var answer = [];
+  const total = brown + yellow;
+  for (let n = 1; n < total; n++) {
+    let m = total / n;
+    if ((m + n) * 2 - 4 === brown && m >= n) answer = [m, n];
+  }
+  return answer;
+}
